a9735a16c58455b30d0d65ee1436c366874e2f98
albertoouu <albertoouu@gmail.com>
2021-10-14 12:09:35 -0500

    creacion de index.js data.js index.html

---
+++ data.js
@@ -0,0 +1,22 @@
+let renderMovies= (movieData) => {
+  console.log(movieData)
+  let catalogue = document.getElementById('catalogue')
+  //let movieName = document.getElementById('movie-name')
+  let movieImg = document.getElementById('movie-img')
+  for(let i =0; i<movieData.Search.length; i++){
+    //movieName.innerHTML = movieData.Search[1].Title
+    //movieImg.setAttribute('src', movieData.Search[1].Poster)
+    console.log(movieData.Search[i].Poster)
+    catalogue.innerHTML += `<h2>${movieData.Search[i].Title}</h2> <img src="${movieData.Search[i].Poster}" alt="movie-img" id="movie-img" />`
+  }
+}
+
+export let getMovieFromOMDBAPI = (searchMovie) => {
+  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ffc12cae&s=${searchMovie}`)
+ .then((response) => response.json())
+ .then( (data) => renderMovies(data))
+ .catch((error) => console.log(error))
+ .finally(() => console.log("promesa resuelta"))
+}
+
+//data.Search[0].Title
---
+++ index.js
@@ -0,0 +1,19 @@
+import {getMovieFromOMDBAPI} from "/data.js"
+
+let searchMovie = () => {
+  let searchValue = document.getElementById('searchInput').value
+  console.log(searchValue)
+  getMovieFromOMDBAPI(searchValue)
+}
+
+let agregarDivs = () => {
+  console.log("agregarDivs")
+  let agregar = document.getElementById('catalogue')
+  for(let i =0; i<5;i++){
+  agregar.innerHTML += `<p>Hooollla</p> <h3> Es la interacion numero: ${i}</h3>`
+  }
+}
+
+let buttonSearch = document.getElementById('searchButton')
+buttonSearch.addEventListener('click',searchMovie)
+
