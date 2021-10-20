614fe67b424c063708f6b9d87954dfd55ed5dd8d
albertoouu <albertoouu@gmail.com>
2021-10-19 23:14:12 -0500

    se añadio sass a el proyecto

--- src/data.js
+++ src/data.js
@@ -1,12 +1,11 @@
-
-const novedades =["midnight sky","sputnik","ad astra","tenet","ready player one","songbird","infinite","the tomorrow war","2067","arrival"]
+const novedades =["the midnight sky","sputnik","ad astra","tenet","ready player one","songbird","infinite","the tomorrow war","2067","arrival"]
 const recomendaciones = ["blade runner","mad max fury road","the matrix","interstellar","a.i.","back to the future","children of men","akira","alien"]
 const clasicas = ["2001","metropolis","the incredible shrinking man","e.t.","Le voyage dans la lune","the thing","blade runner","Close Encounters of the Third Kind"]
 const populares = ["avatar","star wars","avengers endgame","jurassic park","e.t.","independence day","men in black","armageddon"]
 const tendencias = ["inception","ex machina","seconds", "dune","avatar","star wars","snowpiercer","gravity","war of the worlds"]
 const maraton = ["the matrix","the matrix reloaded","the matrix revolutions","blade runner","blade runner 20490","back to the future part II","Back to the Future Part III"]
 
-let renderMovies= (movieData) => {
+let renderSearchMovies= (movieData) => {
   console.log(movieData)
   let catalogue = document.getElementById('catalogue')
   //let movieName = document.getElementById('movie-name')
@@ -18,32 +17,37 @@ let renderMovies= (movieData) => {
     catalogue.innerHTML += `<h2>${movieData.Search[i].Title}</h2> <img src="${movieData.Search[i].Poster}" alt="movie-img" id="modificar con bootsrapp" />`
   }
 }
-let renderMovie= (movieData) => {
+let renderStartMovies= (movieData,div) => {
   console.log(movieData)
-  let catalogue = document.getElementById('catalogue')
-  //let movieName = document.getElementById('movie-name')
-  //let movieImg = document.getElementById('movie-img')
-    //movieName.innerHTML = movieData.Search[1].Title
-    //movieImg.setAttribute('src', movieData.Search[1].Poster)
+  console.log(div)
     console.log(movieData.Poster)
-    catalogue.innerHTML += `<h2>${movieData.Title}</h2>
-    <img src="${movieData.Poster}" alt="movie-img" id="movie-img" />
+    div.innerHTML += `<img src="${movieData.Poster}" alt="movie-img" class="img-fluid" />`
+    /*
+    <h2>${movieData.Title}</h2>
     <h4>Plot:</h4><p>${movieData.Plot}</p>
     <h4>Actors:</h4><p>${movieData.Actors}</p>
-    <h4>Year:</h4><p>${movieData.Year}</p>`
+    <h4>Year:</h4><p>${movieData.Year}</p>
+    */
 }
 
 export let searchMoviesFromOMDBAPI = (searchMovie) => {
   fetch(`https://www.omdbapi.com/?s=${searchMovie}&apikey=ffc12cae&`)
  .then((response) => response.json())
- .then( (data) => renderMovies(data))
+ .then( (data) => renderSearchMovies(data))
  .catch((error) => console.log(error))
  .finally(() => console.log("promesa resuelta"))
 }
-export let printMovieFromOMDBAPI = (searchMovie) => {
-  fetch(`http://www.omdbapi.com/?t=${searchMovie}&apikey=ffc12cae`)
- .then((response) => response.json())
- .then( (data) => renderMovie(data))
- .catch((error) => console.log(error))
- .finally(() => console.log("promesa resuelta"))
+
+export let printMovieFromOMDBAPI = (div,Arr) => {
+  for (let i = 0; i<=(Arr.length-1);i++) {
+    fetch(`http://www.omdbapi.com/?t=${Arr[i]}&apikey=ffc12cae`)
+    .then((response) => response.json())
+    .then( (data) => {
+      console.log(data)
+      renderStartMovies(data,div)
+    })
+    .catch((error) => console.log(error))
+    .finally(() => console.log("promesa resuelta"))
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 import {searchMoviesFromOMDBAPI} from "./data.js"
 import {printMovieFromOMDBAPI} from "./data.js"
 
+const ArrNovedades =["the midnight sky","sputnik","ad astra","tenet","ready player one","songbird","infinite","the tomorrow war","2067","arrival"]
+const ArrRecomendaciones = ["blade runner","mad max fury road","the matrix","interstellar","a.i.","back to the future","children of men","akira","alien"]
+const ArrClasicas = ["2001","metropolis","the incredible shrinking man","e.t.","Le voyage dans la lune","the thing","blade runner","Close Encounters of the Third Kind"]
+const ArrPopulares = ["avatar","star wars","avengers endgame","jurassic park","e.t.","independence day","men in black","armageddon"]
+const ArrTendencias = ["inception","ex machina","seconds", "dune","avatar","star wars","snowpiercer","gravity","war of the worlds"]
+const ArrMaraton = ["the matrix","the matrix reloaded","the matrix revolutions","blade runner","blade runner 2049","back to the future part II","Back to the Future Part III"]
+
 let searchMovie = () => {
   let searchValue = document.getElementById('searchInput').value
   console.log(searchValue)
   searchMoviesFromOMDBAPI(searchValue)
 }
+
 let printStartMovies = () => {
   let novedades = document.getElementById('novedades')
-}
+  printMovieFromOMDBAPI(novedades,ArrNovedades)
 
+  let recomendaciones = document.getElementById('recomendaciones')
+  printMovieFromOMDBAPI(recomendaciones,ArrRecomendaciones)
+
+    let clasicas = document.getElementById('clasicas')
+  printMovieFromOMDBAPI(clasicas,ArrClasicas)
+
+    let populares = document.getElementById('populares')
+  printMovieFromOMDBAPI(populares,ArrPopulares)
+
+    let tendencias = document.getElementById('tendencias')
+  printMovieFromOMDBAPI(tendencias,ArrTendencias)
+
+    let maraton = document.getElementById('maraton')
+  printMovieFromOMDBAPI(maraton,ArrMaraton)
+}
+printStartMovies()
 let buttonSearch = document.getElementById('searchButton')
 buttonSearch.addEventListener('click',searchMovie)
